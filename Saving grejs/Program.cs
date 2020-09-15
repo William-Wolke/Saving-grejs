695432f98279c1ba4759c1cda555616068b5f107
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2020-09-15 13:16:59 +0200

    Initial commit

---
+++ Saving grejs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Saving_grejs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
