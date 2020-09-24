9cb803f627208ee4024d9855f0d36355038d4acd
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2020-09-24 16:27:11 +0200

    Användar input

--- Saving grejs/Program.cs
+++ Saving grejs/Program.cs
@@ -8,7 +8,65 @@ namespace Saving_grejs
     {
         static void Main(string[] args)
         {
-            //MovingPlatform myMovingPlatform = new MovingPlatform;
+            MovingPlatform myMovingPlatform = new MovingPlatform();
+
+            bool isDone = false;
+
+            bool stepOne = false;
+
+            bool stepTwo = false;
+
+            bool stepThree = false;
+
+            string x;
+
+            string y;
+
+            string speed;
+
+            int x2;
+
+            int y2;
+
+            float speed2;
+            
+            while (isDone == false) {
+
+                Console.WriteLine("Vad är plattformens x värde");
+
+                x = Console.ReadLine();
+
+                bool isItANumber = int.TryParse(x, out x2);
+
+                if (isItANumber == true) {
+                    stepOne = true;
+                }
+
+                isItANumber = false;
+
+                Console.WriteLine("Vad är plattformens y värde");
+
+                y = Console.ReadLine();
+
+                isItANumber = int.TryParse(y, out y2);
+
+                if (isItANumber == true) {
+                    stepTwo = true;
+                }
+
+                isItANumber = false;
+
+                 Console.WriteLine("Vad är plattformens hastighet");
+
+                y = Console.ReadLine();
+
+                isItANumber = int.TryParse(y, out y2);
+
+                if (isItANumber == true) {
+                    stepThree = true;
+                }
+
+                if ();
         }
     }
 }
